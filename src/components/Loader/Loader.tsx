6adbdc44a663cdac641a900f00e3f0588cf024e9
Chinwilla <bogdandrozd123@gmail.com>
2024-06-28 20:42:22 +0100

    updata

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,7 +4,12 @@ import css from "./Loader.module.css";
 const Loader: React.FC = () => {
   return (
     <div className={css.loadbox}>
-      <InfinitySpin width={200} color="darkblue" visible={true} ariaLabel="infinity-spin-loading" />
+      <InfinitySpin
+        width="200px"  // Corrigido para string
+        color="darkblue"
+        visible={true}
+        ariaLabel="infinity-spin-loading"
+      />
     </div>
   );
 };
